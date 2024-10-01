62855ae401106ffe9f83d2afc51b7a25a4558943
NachooSR <119146321+NachooSR@users.noreply.github.com>
2024-10-01 15:53:44 -0300

    Finalizando portfolio

--- Logic/logic.js
+++ Logic/logic.js
@@ -1,5 +1,26 @@
-const proyectos = document.querySelectorAll('.proyecto');
+///NAVBAR
 
+function showSideBar(){
+
+    //evitar el reload de la pagina
+    event.preventDefault();
+    const sideBar= document.querySelector('.sideBar')
+    sideBar.style.display='flex'
+}
+
+function hideSideBar(){
+    event.preventDefault();
+    const sideBar= document.querySelector('.sideBar')
+    sideBar.style.display='none'
+}
+
+
+
+
+
+
+/////ZONA PROYECTOS
+const proyectos = document.querySelectorAll('.proyecto');
 
 let proyectoActual = 0;
 
@@ -27,3 +48,18 @@ document.getElementById('backBtn').addEventListener('click', function() {
     proyectoActual = (proyectoActual - 1 + proyectos.length) % proyectos.length;
     mostrarProyecto(proyectoActual);
 });
+
+
+
+///Copiar Mail
+function copiarPortapapeles( text) {
+  event.preventDefault(); 
+  navigator.clipboard.writeText(text)
+
+  const icon=document.getElementById("checkmark");
+  icon.style.color="green";
+   
+  setTimeout(() => {
+    icon.style.color = "";
+  }, 1000);
+}
